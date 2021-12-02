52793097d9415e33680ba17e9b1f0c6e023265a4
Tomas Birbe <tomas.birbe@gmail.com>
2021-12-02 20:35:12 -0300

    Setting up chakra in order to make the UI

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import authContext from '../authContext';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme';
 
 const auth = {
   token: '',
@@ -12,7 +14,9 @@ const auth = {
 ReactDOM.render(
   <authContext.Provider value={auth}>
     <Router>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Router>
   </authContext.Provider>,
   document.getElementById('root')
